946c8570ff5649a640e9f585af5a586149239d76
Aruran05 <aruran654@gmail.com>
2026-03-27 13:05:24 +0100

    Update Enonce_partie_2.py
    Ajout exo 3

--- Enonce_partie_2.py
+++ Enonce_partie_2.py
@@ -80,7 +80,7 @@ def regression_logistique(W, b, X, Y, T, lr=0.1, nb_iter=100, int_affiche=10):
             suite_erreur.append(cross_entropy(Y, T))
     return suite_erreur
 
-
+# Exercice 3
 def initialise(D, K, X):
     W = np.random.uniform(-2, 2, size=(D, K))
     b = np.random.uniform(size=K)
@@ -88,8 +88,6 @@ def initialise(D, K, X):
     print(Y.shape)
     C_train = predit_classe(Y)
     return W, b, Y, C_train
-
-
 def cross_entropy(Y, T):
     J = 0
     N = T.shape[0]
@@ -98,8 +96,6 @@ def cross_entropy(Y, T):
         for k in range(K):
             J += T[n, k] * np.log(Y[n, k])
     return -J
-
-
 def updateWb(W, b, X, Y, T, lr):
     Nabla_W = np.dot(np.transpose(X), (Y-T))
     W[:] = W - lr * Nabla_W
@@ -112,9 +108,109 @@ def taux_precision(Y, T):
         for j in range(Y.shape[1]):
             if Y[i,j] == 1 and T[i,j] == 1:
                 nb_pt += 1
-    return nb_pt / len(C)
+    return nb_pt / len(Y)
+
+def convertit(T, K=5):
+    N = T.shape[0]
+    T_convertit = np.zeros((N, K))
+    for i in range(N):
+        for j in range(K):
+            if T[i, 0] == j:
+                T_convertit[i, j] = 1
+            else:
+                T_convertit[i, j] = 0
+    return T_convertit
+
+def sigma(x):
+    return 1/(1+np.exp(-x))
+
+def softmax(A):
+    n, p = A.shape
+    B = np.zeros((n, p))
+    for i in range(n):
+        somme = 0
+        for z in range(len(A[i, :])):
+            somme += np.exp(A[i, z])
+        for j in range(p):
+            B[i, j] = np.exp(A[i, j]) / somme
+    return B
 
 
+def predit_classe(Y):
+    n, p = Y.shape
+    C = np.zeros((n, p))
+    l_max = Y.max(axis=1)
+    for i in range(n):
+        for j in range(p):
+            if Y[i, j] == l_max[i]:
+                C[i, j] = 1
+            else:
+                C[i, j] = 0
+    return C
+
+def taux_precision(C, T):
+    N = len(T)
+    return np.sum(np.equal(T, C))*100/N
+
+def cross_entropy(Y, T):
+    J = 0
+    N = T.shape[0]
+    K = T.shape[1]
+    for n in range(N):
+        for k in range(K):
+            J += T[n, k] * np.log(Y[n, k])
+    return -J
+
+def affichage(X, T, C):
+    fig,ax = plt.subplots(1,2, figsize = (15,7))
+    # Affichage des vrais classes
+    ax[0].scatter(X[:,0], X[:,1], c=T, s = 40)
+    ax[0].set_title("Classes réelles données par T")
+
+    # Affichage des prédictions
+    ax[1].scatter(X[:,0], X[:,1], c=C, s = 40)
+    ax[1].set_title("Classes prédites données par C")
+    plt.show()
+
+def predit_proba(X, W, b):
+    Z = [X]
+    for i in range (len(b)-1):
+        Z.append(sigma(Z[-1].dot(W[i])+b[i]))
+    Y = softmax(np.dot(Z[-1],W[-1])+b[-1])
+    return Z, Y
+
+def initialise(dimensions) :
+    global N
+    W = []
+    b = []
+    for p in range(len(dimensions)-1) :
+        W.append(np.random.uniform(-2,2,size=(dimensions[p],dimensions[p+1])))
+        b.append(np.random.uniform(-0.5,0.5,size=(dimensions[p+1])))
+    Z,Y = predit_proba(X_train, W, b)
+    C = predit_classe(Y)
+    return W,b,Y,Z,C
+
+def updateWb(W, b, X, Z, Y, T, lr):
+    delta_a_lenvers=[Y-T]
+    for i in range (len(W)-1):
+        W[-1-i] -= lr*(Z[-1-i].transpose()).dot(delta_a_lenvers[-1])
+        b[-1-i] -= lr*sum(delta_a_lenvers[-1])
+        delta_a_lenvers.append(np.dot(delta_a_lenvers[-1], W[-1-i].transpose())*Z[-1-i]*(1-Z[-1-i]))
+    W[0] -= lr*(np.transpose(X)).dot(delta_a_lenvers[-1])
+    b[0] -= lr*sum(delta_a_lenvers[-1])
+    
+def reseau(W, b, X, Z, Y, T, lr=0.1, nb_iter=100, int_affiche=10) :
+    suite_erreur = [cross_entropy(Y,T)]
+    for i in range(nb_iter):
+        updateWb(W ,b ,X ,Z ,Y ,T ,lr)
+        Z[:], Y[:] = predit_proba(X, W, b) #Sans le [:], le tableau ne serait pas modifié
+        if i % int_affiche == 0:
+            erreur_iter = cross_entropy(Y, T)
+            print("Erreur cross_entropy a l'iteration ", i+1 ," : " , erreur_iter)
+            suite_erreur.append(erreur_iter)
+    return suite_erreur
+
+# EXERCICE 2   
 # %% Import du jeu de données : probleme à 4 classes
 X_train, T_train = readdataset2d("probleme_4_classes")
 N, D = X_train.shape
@@ -130,7 +226,6 @@ W_init = W.copy()
 b_init = b.copy()
 suite_erreur = regression_logistique(W, b, X_train, Y_train, T, lr=0.01, int_affiche=10)
 C_train_final = predit_classe(Y_train)
-
 print('\nSituation initiale')
 print("Poids initiaux : ", W_init, b_init)
 print("Taux de précision initial= ", taux_precision(C_train_init, T))
@@ -170,6 +265,8 @@ print("Poids optimises :", W, b)
 print("Erreur d'entropie finale :", suite_erreur[-1])
 print("Taux de précision final = ", taux_precision(C_train_final, T))
 
+
+# Exercice 3 
 # %% Import du jeu de données : probleme à 6 classes
 X_train, T_train = readdataset2d("probleme_5_classes_dur")
 N, D = X_train.shape
@@ -177,3 +274,53 @@ N, D = X_train.shape
 # Pour la visualisation, on garde T_train sous sa forme originelle
 plt.scatter(X_train[:, 0], X_train[:, 1], c=T_train, s=30)
 plt.show()
+
+dimensions5 = [2,15,15,3,5]
+
+W,b,Y,Z,C_init = initialise(dimensions5)
+W_init = W.copy()
+b_init = b.copy()
+
+suite_erreur = reseau_dense(W, b, X_train, Z, Y, T_train,lr=0.001,nb_iter=10000)
+C_train_final = predit_classe(Y)
+
+print('\nSituation initiale')
+print("Poids initiaux : ", W_init,b_init)
+print("Taux de précision initial= ", taux_precision(C_init, T_train))
+print("Erreur d'entropie initiale :", suite_erreur[0])
+
+
+print('\nRésultats')
+print("Poids optimises :", W, b)
+print("Erreur d'entropie finale :", suite_erreur[-1])
+print("Taux de précision final = ", taux_precision(C_train_final, T_train))
+affichage(X_train, T_train, C_train_final)
+
+X_train, T_train = readdataset2d("probleme_5_classes_dur")
+N, D = X_train.shape
+
+# Pour la visualisation, on garde T_train sous sa forme originelle
+plt.scatter(X_train[:, 0], X_train[:, 1], c=T_train, s=30)
+plt.show()
+dimension_5=[2, 15, 15, 3, 5]
+
+W,b,Y,Z,C_init = initialise(dimension_5)
+W_init = W.copy()
+b_init = b.copy()
+
+T = convertit(T_train)
+suite_erreur = reseau(W, b, X_train, Z, Y, T,lr=0.001,nb_iter=1000)
+C_train_final = predit_classe(Y)
+print(C_train_final.shape,T_train.shape,X_train.shape)
+
+print('\nSituation initiale')
+print("Poids initiaux : ", W_init,b_init)
+print("Taux de précision initial= ", taux_precision(C_init, T_train))
+print("Erreur d'entropie initiale :", suite_erreur[0])
+
+
+print('\nRésultats')
+print("Poids optimises :", W, b)
+print("Erreur d'entropie finale :", suite_erreur[-1])
+print("Taux de précision final = ", taux_precision(C_train_final, T_train))
+
